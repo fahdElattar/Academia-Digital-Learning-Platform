23c49b2efe23fd644f58effe2d59ee7a5e626d4d
fahdElattar <fahdelattar13@gmail.com>
2024-06-02 09:07:21 +0000

    solving the sidebar fixed problem

--- client/src/Components/StarterPage.jsx
+++ client/src/Components/StarterPage.jsx
@@ -5,10 +5,10 @@ import Sidebar from './Sidebar'
 const StarterPage = ({ children }) => {
   return (
     <div className='min-vh-100 w-100 p-0 m-0 d-flex'>
-        <div className="p-0 m-0" style={{width: '21%'}}>
+        <div className="p-0 m-0 fixed-top h-100" style={{width: '21%'}}>
           <Sidebar />
         </div>
-          <div className="" style={{width: '79%', padding: '0 2.6rem'}}>
+          <div className="" style={{width: '79%', marginLeft:'21%', padding: '0 2.6rem'}}>
               <Navbar />
               {children}
           </div>
